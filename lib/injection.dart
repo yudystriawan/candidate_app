53091700149bf8b1c12f024707dfa347ebb958cb
yudystriawan <yudistriawan@gmail.com>
2023-07-13 10:50:17 +0800

    add injection

---
+++ lib/injection.dart
@@ -0,0 +1,10 @@
+import 'package:candidate_app/injection.config.dart';
+import 'package:get_it/get_it.dart';
+import 'package:injectable/injectable.dart';
+
+final getIt = GetIt.instance;
+
+@InjectableInit(
+  preferRelativeImports: true,
+)
+void configureDependencies() => getIt.init();

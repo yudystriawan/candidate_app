08f1b79538561a9f36aba091a58a7f5bcead9e24
yudystriawan <yudistriawan@gmail.com>
2023-07-13 10:50:20 +0800

    fix code

--- lib/core/usecases/usecase.dart
+++ lib/core/usecases/usecase.dart
@@ -1,13 +1,11 @@
 import 'package:dartz/dartz.dart';
 import 'package:equatable/equatable.dart';
-import 'package:freezed_annotation/freezed_annotation.dart';
 import '../error/failures.dart';
 
 abstract class Usecase<Type, Params> {
   Future<Either<Failure, Type>> call(Params params);
 }
 
-@freezed
 class NoParams extends Equatable {
   @override
   List<Object?> get props => [];

5b8f46e6b817ac6081f971294dac9d5863505804
yudystriawan <yudistriawan@gmail.com>
2023-07-13 12:16:29 +0800

    add auto route

---
+++ lib/routes/router.dart
@@ -0,0 +1,12 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:candidate_app/routes/router.gr.dart';
+
+@AutoRouterConfig()
+class AppRouter extends $AppRouter {
+  @override
+  List<AutoRoute> get routes {
+    return [
+      AutoRoute(page: CandidateRoute.page),
+    ];
+  }
+}
